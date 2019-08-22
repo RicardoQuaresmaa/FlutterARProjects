28c24cbda34237ec37f73da4c7ab2ac8ead66c28
RicardoQuaresma <bunyamineymen@gmail.com>
2019-08-22 13:47:16 +0300

    123

---
+++ arcore-flutter-master/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <arcore_flutter_plugin/ArcoreFlutterPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ArcoreFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"ArcoreFlutterPlugin"]];
+}
+
+@end
